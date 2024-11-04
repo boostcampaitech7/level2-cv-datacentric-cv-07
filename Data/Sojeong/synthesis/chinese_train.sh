b31d56b4f2240e1c71129b6560e411f9824362b1
Soojeoong <soojeoong43@gmail.com>
2024-11-04 14:54:49 +0900

    Feature: --- data, create 200 images per language

---
+++ Data/Sojeong/synthesis/chinese_train.sh
@@ -0,0 +1,8 @@
+python data_synthesis.py\
+    --output_dir "./synth_data/chinese_receipt" \
+    --json_path "/data/ephemeral/home/data/chinese_receipt/ufo/train.json" \
+    --language "ch" \
+    --fonts "/data/ephemeral/home/Sojeong/level2-cv-datacentric-cv-07/Data/Sojeong/synthesis/font/cn/ChillHuoFangKai_F_ConBold.otf" "/data/ephemeral/home/Sojeong/level2-cv-datacentric-cv-07/Data/Sojeong/synthesis/font/cn/ChillHuoKai_ConRegular.otf" "/data/ephemeral/home/Sojeong/level2-cv-datacentric-cv-07/Data/Sojeong/synthesis/font/cn/ChillHuoSong_F_ExBold.otf" "/data/ephemeral/home/Sojeong/level2-cv-datacentric-cv-07/Data/Sojeong/synthesis/font/cn/Slideyouran-Regular.ttf" "/data/ephemeral/home/Sojeong/level2-cv-datacentric-cv-07/Data/Sojeong/synthesis/font/cn/SourceHanSans-Normal.ttf"\
+
+# chmod +x ./chinese_train.sh
+# ./chinese_train.sh
---
+++ Data/Sojeong/synthesis/thai_train.sh
@@ -0,0 +1,9 @@
+python data_synthesis.py\
+    --output_dir "./synth_data/thai_receipt" \
+    --json_path "/data/ephemeral/home/data/thai_receipt/ufo/train.json" \
+    --language "th" \
+    --fonts "/data/ephemeral/home/Sojeong/level2-cv-datacentric-cv-07/Data/Sojeong/synthesis/font/th/Athiti-Bold.ttf" "/data/ephemeral/home/Sojeong/level2-cv-datacentric-cv-07/Data/Sojeong/synthesis/font/th/BaiJamjuree-Italic.ttf" "/data/ephemeral/home/Sojeong/level2-cv-datacentric-cv-07/Data/Sojeong/synthesis/font/th/ChakraPetch-Medium.ttf" "/data/ephemeral/home/Sojeong/level2-cv-datacentric-cv-07/Data/Sojeong/synthesis/font/th/Fahkwang-Regular.ttf" "/data/ephemeral/home/Sojeong/level2-cv-datacentric-cv-07/Data/Sojeong/synthesis/font/th/Taviraj-ExtraLight.ttf"\
+
+
+# chmod +x ./thai_train.sh
+# ./thai_train.sh
